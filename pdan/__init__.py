e1f99d95a01c0f90e52679fe9ce0d5da2070dd4b
GeorgySk <skorobogatov@phystech.edu>
2022-12-25 16:06:23 +0100

    update imports and add usage example

--- pdan/__init__.py
+++ pdan/__init__.py
@@ -1,3 +1,8 @@
 """Polygon decomposition"""
 
 __version__ = '0.1.1-alpha'
+
+from .pdan import (minimizing_split,
+                   Contour,
+                   Point,
+                   Polygon)
